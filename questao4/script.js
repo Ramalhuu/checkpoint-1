3079c43515ce5cca31cb7e6ac9a4756e8395c9c3
Felipe Ramalho <felipejunqueira@yahoo.com>
2025-04-09 13:45:22 -0300

    Questão 4 - Variáveis

---
+++ questao4/script.js
@@ -0,0 +1,15 @@
+var a = 1
+var b = 2
+
+let nome = "nome"
+let sobrenome = "sobrenome"
+
+const pi = 3.14
+const nomeverdadeiro = "felipe"
+
+console.log("O valor inicial de a: ", a)
+console.log("O valor inicial de b: ", b)
+console.log("O valor inicial de nome: ", nome)
+console.log("O valor inicial de sobrenome: ", sobrenome)
+console.log("O valor inicial de pi: ", pi)
+console.log("O valor inicial de nome verdadeiro: ", nomeverdadeiro)
