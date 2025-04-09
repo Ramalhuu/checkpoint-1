a6ea7aea9caeaa54dc4baa03eb4461334533cdc8
Felipe Ramalho <felipejunqueira@yahoo.com>
2025-04-09 13:57:41 -0300

    Questão 6 - Operadores de Comparação

---
+++ questao6/script.js
@@ -0,0 +1,12 @@
+let numero = 3
+
+
+console.log(numero == 3, numero == 4)
+console.log(numero === "3", numero === 3)
+console.log(numero != 3, numero != 4)
+console.log(numero !== 3, numero !== "3")
+console.log(numero <= 4)
+console.log(numero >= 4)
+console.log(6 > numero > 2)
+
+
