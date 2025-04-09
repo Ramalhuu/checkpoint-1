985a85201004442c148f6c5323dfa640924ae95a
Felipe Ramalho <felipejunqueira@yahoo.com>
2025-04-09 14:21:18 -0300

    Questão 10 – Operadores no Navegador

---
+++ questao10/script.js
@@ -0,0 +1,10 @@
+let num1 = 5
+let num2 = 10
+
+console.log("Soma: ", num1 + num2)
+
+let estaChovendo = true
+let tenhoGuardaChuva = false
+
+console.log("Resultado de &&:", estaChovendo && tenhoGuardaChuva) 
+console.log("Resultado de ||:", estaChovendo || tenhoGuardaChuva)
